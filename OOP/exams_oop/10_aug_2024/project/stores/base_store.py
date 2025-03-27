30883f53ef8c8dd2a7bdeee7306e84ffd0f3178b
HrisKirova <kirovahristina@yahoo.com>
2025-03-27 22:41:07 +0200

    final version

---
+++ OOP/exams_oop/10_aug_2024/project/stores/base_store.py
@@ -0,0 +1,40 @@
+from abc import ABC, abstractmethod
+
+
+class BaseStore(ABC):
+    def __init__(self, name: str, location: str, capacity: int):
+        self.name = name
+        self.location = location
+        self.capacity = capacity
+        self.products = []
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, value):
+        if value.strip() == "":
+            raise ValueError("Store name cannot be empty!")
+        self.__name = value
+
+    @property
+    def location(self):
+        return self.__location
+
+    @location.setter
+    def location(self, value):
+        if len(value.strip()) != 3 or ' ' in value:
+            raise ValueError("Store location must be 3 chars long!")
+        self.__location = value
+
+    @property
+    def capacity(self):
+        return self.__capacity
+
+    @capacity.setter
+    def capacity(self, value):
+        if value < 0:
+            raise ValueError("Store capacity must be a positive number or 0!")
+        self.__capacity = value
+
